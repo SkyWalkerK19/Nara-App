fc41ff08ba7ac33cf87727d690063507c3f6f07a
Khyran Bush <Khyranbush@gmail.com~>
2025-09-05 13:04:33 -0400

    Changes to guard

--- types/guards.ts
+++ types/guards.ts
@@ -2,17 +2,18 @@
 
 import type { Chat } from './Chat';
 import type { User } from './User';
-
 /**
  * Checks if the given chat is a direct (1:1) chat.
  */
+
 export function isDirectChat(chat: Chat): chat is Chat {
   return chat.is_group === false;
 }
-
 /**
  * Checks if the user has the admin role.
  */
 export function isAdminUser(user: User | null | undefined): user is User {
   return !!user && user.role === 'admin';
 }
+
+
